6454811bbdf4f844612522f768d19d6bb86f28f7
Sergey Cheperis <czz@czz.name>
2021-09-30 17:24:41 +0300

    Use nclient6

--- src/dhcpv6.go
+++ src/dhcpv6.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -8,7 +9,7 @@ import (
 	"time"
 
 	"github.com/insomniacslk/dhcp/dhcpv6"
-	"github.com/insomniacslk/dhcp/dhcpv6/client6"
+	"github.com/insomniacslk/dhcp/dhcpv6/nclient6"
 	"github.com/insomniacslk/dhcp/iana"
 )
 
@@ -26,96 +27,25 @@ func receiveConfWithDhcp(ifname string) (NetConf, error) {
 		return NetConf{}, err
 	}
 	log.Printf("DHCPv6 prefix delegation on interface %s (%s)", iface.Name, iface.HardwareAddr.String())
-	attempts := 3
-	var lastErr error
-	for attempt := 0; attempt < attempts; attempt++ {
-		log.Printf("Attempt %d of %d", attempt+1, attempts)
-		conf, err := tryReceiveConfWithDhcp(iface)
-		if err == nil {
-			return conf, nil
-		} else {
-			log.Printf("Error: %v", err)
-			lastErr = err
-		}
-	}
-	return NetConf{}, lastErr
-}
 
-func tryReceiveConfWithDhcp(iface *net.Interface) (NetConf, error) {
-	c, err := createClient(iface)
-	advertise, err := doSolicit(c, iface)
+	// fix for "sendto: no route to host"
+	nclient6.AllDHCPRelayAgentsAndServers = &net.UDPAddr{
+		IP:   net.ParseIP("ff02::1:2"),
+		Port: dhcpv6.DefaultServerPort,
+		Zone: ifname,
+	}
+
+	client, err := nclient6.New(ifname, nclient6.WithDebugLogger())
 	if err != nil {
 		return NetConf{}, err
 	}
-	reply, err := doRequest(c, iface, advertise)
+	defer client.Close()
+	ctx := context.Background()
+	reply, err := client.RapidSolicit(ctx, getModifiers(iface)...)
 	if err != nil {
 		return NetConf{}, err
 	}
 	return netConfFromReply(reply)
-}
-
-func createClient(iface *net.Interface) (*client6.Client, error) {
-	llAddr, err := dhcpv6.GetLinkLocalAddr(iface.Name)
-	if err != nil {
-		return nil, err
-	}
-	laddr := net.UDPAddr{
-		IP:   llAddr,
-		Port: dhcpv6.DefaultClientPort,
-		Zone: iface.Name,
-	}
-	raddr := net.UDPAddr{
-		IP:   dhcpv6.AllDHCPRelayAgentsAndServers,
-		Port: dhcpv6.DefaultServerPort,
-		Zone: iface.Name,
-	}
-	c := client6.NewClient()
-	c.LocalAddr = &laddr
-	c.RemoteAddr = &raddr
-	return c, nil
-}
-
-func doSolicit(c *client6.Client, iface *net.Interface) (dhcpv6.DHCPv6, error) {
-	solicit, advertise, err := c.Solicit(iface.Name, getModifiers(iface)...)
-	if solicit != nil {
-		printSent(solicit)
-	}
-	if advertise != nil {
-		printReceived(advertise)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	// Decapsulate advertise if it's relayed before passing it to Request
-	if advertise.IsRelay() {
-		advertiseRelay := advertise.(*dhcpv6.RelayMessage)
-		advertise, err = advertiseRelay.GetInnerMessage()
-		if err != nil {
-			return nil, err
-		}
-		printReceived(advertise)
-	}
-
-	return advertise, nil
-}
-
-func doRequest(c *client6.Client, iface *net.Interface, advertise dhcpv6.DHCPv6) (dhcpv6.DHCPv6, error) {
-	request, reply, err := c.Request(
-		iface.Name,
-		advertise.(*dhcpv6.Message),
-		getModifiers(iface)...,
-	)
-	if request != nil {
-		printSent(request)
-	}
-	if reply != nil {
-		printReceived(reply)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return reply, nil
 }
 
 func getModifiers(iface *net.Interface) []dhcpv6.Modifier {
@@ -176,14 +106,6 @@ func netConfFromReply(reply dhcpv6.DHCPv6) (NetConf, error) {
 		return conf, nil
 	}
 	return NetConf{}, errors.New("No prefixes returned in IA PD")
-}
-
-func printSent(m dhcpv6.DHCPv6) {
-	log.Print("-> ", m.Summary())
-}
-
-func printReceived(m dhcpv6.DHCPv6) {
-	log.Print("<- ", m.Summary())
 }
 
 func (conf *NetConf) String() string {
