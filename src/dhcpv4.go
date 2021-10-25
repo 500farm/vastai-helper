f12ddad1b6afb3d6bed02896f7bc0ad6a298ec22
Sergey Cheperis <czz@czz.name>
2021-10-25 19:10:23 +0300

    dhcpLeaseV4()

---
+++ src/dhcpv4.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/insomniacslk/dhcp/dhcpv4/nclient4"
+)
+
+func dhcpLeaseV4(ctx context.Context, ifname string, clientId string, hostName string) (NetConf, error) {
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		return NetConf{}, err
+	}
+	log.WithFields(log.Fields{"iface": iface.Name, "clientid": clientId}).
+		Info("DHCPv4 IP request")
+
+	client, err := nclient4.New(ifname, nclient4.WithDebugLogger())
+	if err != nil {
+		return NetConf{}, err
+	}
+	defer client.Close()
+	reply, err := client.Request(
+		ctx,
+		dhcpv4.WithOption(dhcpv4.OptClientIdentifier([]byte(clientId))),
+		dhcpv4.WithOption(dhcpv4.OptHostName(hostName)),
+	)
+	if err != nil {
+		return NetConf{}, err
+	}
+	return netConfFromReplyV4(reply.ACK)
+}
+
+func netConfFromReplyV4(reply *dhcpv4.DHCPv4) (NetConf, error) {
+	ttl := reply.IPAddressLeaseTime(time.Hour)
+	routers := reply.Router()
+	if len(routers) == 0 {
+		return NetConf{}, errors.New("No routers in DHCPv4 lease")
+	}
+	conf := NetConf{
+		mode: None,
+		v4: NetConfPrefix{
+			prefix: net.IPNet{
+				IP:   reply.YourIPAddr,
+				Mask: reply.SubnetMask(),
+			},
+			gateway:           routers[0],
+			preferredLifetime: ttl / 2,
+			validLifetime:     ttl,
+		},
+		dnsServers: reply.DNS(),
+	}
+	search := reply.DomainSearch()
+	if search != nil {
+		conf.dnsSearchList = search.Labels
+	}
+	return conf, nil
+}
