1e9ace9f963d11d4686d3ee5ad263bf98d908a72
Sergey Cheperis <czz@czz.name>
2021-10-25 19:09:42 +0300

    NetConfPrefix logFields()

--- src/netconf.go
+++ src/netconf.go
@@ -42,16 +42,30 @@ func gwAddress(prefix net.IPNet) net.IP {
 	return result
 }
 
+func (p *NetConfPrefix) logFields() log.Fields {
+	return log.Fields{
+		"prefix":  p.prefix.String(),
+		"gw":      p.gateway.String(),
+		"preflt":  p.preferredLifetime,
+		"validlt": p.validLifetime,
+	}
+}
+
 func (conf *NetConf) logFields() log.Fields {
 	return log.Fields{
-		"mode":       conf.mode,
+		"mode": conf.mode,
+
 		"v6.prefix":  conf.v6.prefix.String(),
+		"v6.gw":      conf.v6.gateway.String(),
 		"v6.preflt":  conf.v6.preferredLifetime,
 		"v6.validlt": conf.v6.validLifetime,
-		"v6.gw":      conf.v6.gateway.String(),
+
 		"v4.prefix":  conf.v4.prefix.String(),
 		"v4.gw":      conf.v4.gateway.String(),
-		"dns":        conf.dnsServers,
-		"search":     conf.dnsSearchList,
+		"v4.preflt":  conf.v4.preferredLifetime,
+		"v4.validlt": conf.v4.validLifetime,
+
+		"dns":    conf.dnsServers,
+		"search": conf.dnsSearchList,
 	}
 }
